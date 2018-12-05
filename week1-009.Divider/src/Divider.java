d6ed2b160c14d97f1496f88d1c0faa4a15842733
Denys <denys.zhovnerovych@gmail.com>
2018-12-05 13:04:13 +0200

    grammar*

--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -9,7 +9,7 @@ public class Divider {
         // Implement your program here. Remember to ask the input from user.
         System.out.println("Type a number: ");
         int number1 = Integer.parseInt(reader.nextLine());
-        System.out.println("Type another nubmer: ");
+        System.out.println("Type another number: ");
         int number2 = Integer.parseInt(reader.nextLine());
         double result = 1.0 * number1 / number2;
         System.out.println("Division: " + number1 + "/" + number2 + " = " + result);
